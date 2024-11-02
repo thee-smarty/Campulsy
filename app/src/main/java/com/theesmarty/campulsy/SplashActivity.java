253668050286fddaad46b44c1c0fe61c3d7887e6
The-Smarty <phanipraharsha25@gmail.com>
2024-11-02 19:13:12 +0530

    Project created

---
+++ app/src/main/java/com/theesmarty/campulsy/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.theesmarty.campulsy;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent in = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(in);
+            finish();
+        },3000);
+    }
+}
