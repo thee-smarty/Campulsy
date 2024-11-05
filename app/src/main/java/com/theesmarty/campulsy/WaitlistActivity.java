c517cb20e1aea302cabf6008ab0dda09570c2f85
The-Smarty <phanipraharsha25@gmail.com>
2024-11-05 20:22:45 +0530

    Some Mod in Waitlist Activity

--- app/src/main/java/com/theesmarty/campulsy/WaitlistActivity.java
+++ app/src/main/java/com/theesmarty/campulsy/WaitlistActivity.java
@@ -14,8 +14,12 @@ public class WaitlistActivity extends AppCompatActivity {
 
         // TODO: 11/3/24 if userStat value is 1 then allow user to fill the form
 
-        // TODO: 11/3/24 if userStat vlaue is 2 then Show the user waitlist info and invitation link
+        // TODO: 11/3/24 if userStat value is 2 then Show the user waitlist info and invitation link
+
+        // TODO: 11/5/24 if user fills all he details and click submit, show him alert box with info to join waitlist
 
         // TODO: 11/3/24 if user submits the  form make the shared preferences userStat value as 2
+
+        // TODO: 11/5/24  Generate a link to invite friends for quick adding of campus
     }
 }
